c8c90ab55ca2ed2c44e1c57be57d8b39318410eb
PARMINDER  PATIAL <49855829+parmpatial@users.noreply.github.com>
2026-06-26 15:31:10 +0530

    5.2

---
+++ Exercise 5.2/readme.md
@@ -0,0 +1,17 @@
+## Display Highest Score
+
+# Instruction
+
+Write a program to display highest marks available in the list.
+
+e.g. student_marks = [78, 85, 65, 92, 88]
+
+# Example Input
+
+ <pre>78  85  65  92  88 </pre>
+
+# Example Output
+
+The highest score in all subjects is : 92
+
+# Solution in solution.py
---
+++ Exercise 5.2/solution.py
@@ -0,0 +1,11 @@
+# Don't Change code below line 
+student_marks = [int(x) for x in input("Enter a list of students marks ").split()]
+print(student_marks)
+# Don't Change code above line
+# Write your code below this line
+highest_marks = 0
+for marks in student_marks:
+  if marks > highest_marks:
+    highest_marks = marks
+
+print(f"The highest marks in all subject is : {highest_marks}")
