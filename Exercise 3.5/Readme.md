4ffda949dda688d184c573d53ab5e25dcaef33ad
PARMINDER  PATIAL <49855829+parmpatial@users.noreply.github.com>
2026-06-26 14:56:30 +0530

    3.5

---
+++ Exercise 3.5/Readme.md
@@ -0,0 +1,15 @@
+## Difficult Challange
+
+# Instruction
+
+Write a program to input a number value then check and display entered number is a "Single","Double" digit number or "Neither Single nor Double" digit.
+
+# Example Input
+
+29
+
+# Example Input
+
+Number is a "Double" digit
+
+# Solution in solution.py
---
+++ Exercise 3.5/solution.py
@@ -0,0 +1,10 @@
+# Don't change the code below line
+num = int(input("Enter a number value: "))
+# Don't change the code above line
+# Write your code below this line
+if num >= 0 and num <= 9:
+    print('Number is a "Single" digit')
+elif num >= 10 and num <= 99:
+    print('Number is a "Double" digit')
+else:
+    print('Number is a "Neither Single nor Double" digit')
